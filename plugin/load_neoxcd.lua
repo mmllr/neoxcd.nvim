d5c1f71015f8ad724f124b5c1325ed63e854080a
Markus Müller <mmlr@gmx.net>
2025-01-04 17:16:48 +0100

    Renames user command for selecting a scheme

--- plugin/load_neoxcd.lua
+++ plugin/load_neoxcd.lua
@@ -1,3 +1,4 @@
-vim.api.nvim_create_user_command("SelectXcodeScheme", function()
-	require("neoxcd").select_schemes()
+local neoxcd = require("neoxcd")
+vim.api.nvim_create_user_command("NeoxcdSelectScheme", function()
+	neoxcd.select_schemes()
 end, {})
