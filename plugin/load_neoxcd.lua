ebbf18051d2ad8e98062ea80ec94dfa90ff99685
Markus Müller <mmlr@gmx.net>
2025-01-18 00:41:07 +0100

    Changes commands to lazy load the plugin

--- plugin/load_neoxcd.lua
+++ plugin/load_neoxcd.lua
@@ -1,16 +1,15 @@
-local neoxcd = require("neoxcd")
 vim.api.nvim_create_user_command("NeoxcdSelectScheme", function()
-	neoxcd.select_schemes()
+  require("neoxcd").select_schemes()
 end, {})
 
 vim.api.nvim_create_user_command("NeoxcdSelectDestination", function()
-	neoxcd.select_destination()
+  require("neoxcd").select_destination()
 end, {})
 
 vim.api.nvim_create_user_command("NeoxcdClean", function()
-	neoxcd.clean()
+  require("neoxcd").clean()
 end, {})
 
 vim.api.nvim_create_user_command("NeoxcdBuild", function()
-	neoxcd.build()
+  require("neoxcd").build()
 end, {})
